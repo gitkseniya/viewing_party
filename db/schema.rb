836cfaead6be00a04ee9289e33b69a01b286b840
Brisa Garcia <brisagarciaglz@gmail.com>
2021-05-19 12:20:47 -0600

    Add column name

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_19_160628) do
+ActiveRecord::Schema.define(version: 2021_05_19_175038) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 2021_05_19_160628) do
     t.integer "api_movie_id"
     t.bigint "host_id"
     t.integer "duration"
+    t.string "name"
     t.index ["host_id"], name: "index_parties_on_host_id"
   end
 
