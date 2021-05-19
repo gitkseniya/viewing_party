7585edd93c70d4c9afdad9682f754750932f3c63
Brisa Garcia <brisagarciaglz@gmail.com>
2021-05-19 12:21:25 -0600

    Create Party with time valid time duration edge case point

--- app/controllers/parties_controller.rb
+++ app/controllers/parties_controller.rb
@@ -1,36 +1,36 @@
 class PartiesController < ApplicationController
-  before_action :find_movie
-  helper_method :duration_time_check
+  # before_action :find_movie
+  # helper_method :duration_time_check
 
   def new
     @movie = MovieService.movie_info(params[:api_movie_id])
   end
 
   def create
+
     @movie = MovieService.movie_info(params[:api_movie_id])
-    current_user.parties.create(
-                                  api_movie_id: params[:api_movie_id],
-                                  start_time: params[:start_time],
-                                  duration: params[:duration],
-                                  host_id: current_user.id,
-                                 )
-
-    redirect_to dashboard_path
-
+    return if duration_time_check
+    @party = current_user.parties.create(party_params)
+    if @party.save
+      redirect_to dashboard_path
+    else
+      render :new
+    end
   end
 
   private
+
+  def party_params
+    params.permit(:name, :api_movie_id, :start_time, :duration, :host_id)
+  end
 
   def find_movie
     @movie = MovieService.movie_info(params[:api_movie_id])
   end
 
   def duration_time_check
-    if params[:duration] < @movie[:duration]
-      return
-        flash[:error] = "Party cannot be shorter than the movie"
-        render :new
-      else
-    end
-  end
+    return unless params[:duration].to_i < @movie[:runtime].to_i
+    flash[:error] = "Party cannot be shorter than the movie"
+    render :new
+end
 end
