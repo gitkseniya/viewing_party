30ca19a8a614815b2091de2b8dc4f86608a23752
Kseniya K <kseniya.kudelich@gmail.com>
2021-05-18 21:39:59 -0600

    Add empty field conditional

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,19 +1,22 @@
 class MoviesController < ApplicationController
 
   def index
-    if params[:search]
+    if !params[:search].nil?
+      redirect_to discover_path
+      flash[:notice] = "Please fill in movie title"
+    elsif
+      params[:search]
       @movies = MovieService.search_result(params[:search])
     elsif params[:top_40]
       @movies = MovieService.top_40
-    elsif params[:upcoming_movies]
+    else params[:upcoming_movies]
       @movies = MovieService.upcoming_movies
-    else
-      redirect_to discover_path
     end
   end
 
   def show
     @movie = MovieService.movie_info(params[:id])
+    # @movie = MovieFacade.movie_info(params[:id])
     @casts = MovieService.retrieve_cast(params[:id])
     @reviews = MovieService.retrieve_reviews(params[:id])
     @alternative_titles = MovieService.alternative_titles(params[:id])
