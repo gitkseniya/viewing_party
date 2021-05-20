6fe073b6253da37fa27ab85beb27b8fb3c230eb7
Brisa Garcia <brisagarciaglz@gmail.com>
2021-05-19 22:10:51 -0600

    Clean up MVC

--- app/models/party.rb
+++ app/models/party.rb
@@ -1,5 +1,5 @@
 class Party < ApplicationRecord
   belongs_to :host, class_name: 'User'
-  has_many :guest_lists
+  has_many :guest_lists, dependent: nil
   has_many :friends, through: :guest_lists, class_name: 'User'
 end
