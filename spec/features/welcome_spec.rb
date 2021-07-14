38706bdc31b40cb268b84c99e55280ebb98447e8
Kseniya K <kseniya.kudelich@gmail.com>
2021-07-14 16:49:27 -0600

    WIP

--- spec/features/welcome_spec.rb
+++ spec/features/welcome_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe 'welcome index page' do
   it 'shows welcome info' do
     visit root_path
     expect(page).to have_content('Welcome!')
-    expect(page).to have_content('This is the description')
+    expect(page).to have_content('This is a Viewing Party')
     expect(page).to have_link('Log In')
     expect(page).to have_link('Register As A User')
   end
